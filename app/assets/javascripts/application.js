ec5dcc20dd72580b5f8d2d3adb7907fcc4090a6d
kyohei775 <mr.rad.ok1201@gmail.com>
2021-04-19 07:04:46 +0000

    [add]ページトップ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,3 +44,11 @@ $(document).ready(function () {
   });
 });
 
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
