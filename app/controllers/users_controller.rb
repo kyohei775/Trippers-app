ea59683f5fd2fb558c18ea266401e8bcca435ebd
kyohei775 <mr.rad.ok1201@gmail.com>
2021-04-20 10:12:17 +0000

    [add]フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @post_image = PostImage.new
+  end
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page]).reverse_order
