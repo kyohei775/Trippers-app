bd6352ba7e00bc80510ede6a6d3c7fbfa5bb43fc
kyohei775 <mr.rad.ok1201@gmail.com>
2021-05-12 07:15:07 +0000

    [edit]controller

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -24,22 +24,22 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_image = PostImage.find(params[:id])
+    # @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
 
   def edit
-    @post_image = PostImage.find(params[:id])
+    # @post_image = PostImage.find(params[:id])
   end
 
   def update
-    @post_image = PostImage.find(params[:id])
+    # @post_image = PostImage.find(params[:id])
     @post_image.update(post_image_params)
     redirect_to post_image_path(@post_image.id)
   end
 
   def destroy
-    @post_image = PostImage.find(params[:id])
+    # @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
   end
